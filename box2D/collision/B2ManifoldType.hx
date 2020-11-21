b1037a15181ad71c77d8a0bb8518e68e095fa278
Kevin Brennan <25809479+kmgb@users.noreply.github.com>
2020-11-21 01:10:14 -0500

    Format all Haxe source

--- box2D/collision/B2ManifoldType.hx
+++ box2D/collision/B2ManifoldType.hx
@@ -1,10 +1,7 @@
 package box2D.collision;
 
-
 enum B2ManifoldType {
-	
 	CIRCLES;
 	FACE_A;
 	FACE_B;
-	
 }
--- box2D/collision/B2SeparationFunctionType.hx
+++ box2D/collision/B2SeparationFunctionType.hx
@@ -1,10 +1,7 @@
 package box2D.collision;
 
-
 enum B2SeparationFunctionType {
-	
 	POINTS;
 	FACE_A;
 	FACE_B;
-	
 }
